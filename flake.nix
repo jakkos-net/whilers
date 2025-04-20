f8f28630096b4f89f3fe06381f6f406556f400e2
jakkos-net <45759112+jakkos-net@users.noreply.github.com>
2025-04-20 16:38:17 +0100

    clean up flake

--- flake.nix
+++ flake.nix
@@ -1,53 +1,42 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
+  outputs = {nixpkgs, rust-overlay, ...}: let
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    overlays = [ (import rust-overlay) ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs {inherit system overlays;}));
+  in 
+  {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+          just
+          bacon
+          gcc
+          openssl
+          pkg-config
+          libGL
+          cmake
+          glib
+          fontconfig
+          atk
+          gtk3
+          trunk
+        ];
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay}:
-    let overlays = [
-        (import rust-overlay)
-      ];
-    in
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit overlays system; };
-        lib = pkgs.lib;
-      in 
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            (rust-bin.stable."1.78.0".default.override {
-              extensions = [ "rust-src" "rust-analyzer" ];
-              targets = [ "wasm32-unknown-unknown" ];
-            })
-            just
-            bacon
-            gcc
-            openssl
-            pkg-config
-            libGL
-            cmake
-            glib
-            fontconfig
-            atk
-            gtk3
-            trunk
-          ];
-
-          LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
-            libGL
-            libxkbcommon
-            wayland
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXrandr
-          ];
-        };
-      });
+        LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+          libGL
+          libxkbcommon
+          wayland
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+        ];
+      };
+    });
+  };
 }
